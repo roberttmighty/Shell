85d21e0a8c4b1e29a7b64012ffa37415c9f20bf8
roberttmighty <62709463+roberttmighty@users.noreply.github.com>
2021-01-20 09:00:37 -0500

    Update and rename s_ClearAppCache.sh to ClearAppCache.sh
    Clears Application Domain Cache for PeopleSoft Environments

---
+++ ClearAppCache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /oracle/PS_CFG_HOME/$1/appserv/"$1"/CACHE/*
+rm -rf /oracle/PS_CFG_HOME/$1/appserv/"$1"a/CACHE/*
+rm -rf /oracle/PS_CFG_HOME/$1/appserv/"$1"b/CACHE/*
+rm -rf /oracle/PS_CFG_HOME/$1/appserv/"$1"c/CACHE/*
