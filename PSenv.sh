e466dc23a9b84db53643bd0c704ff14908134987
roberttmighty <62709463+roberttmighty@users.noreply.github.com>
2022-03-15 08:32:06 -0400

    Update PSenv.sh

--- PSenv.sh
+++ PSenv.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-#Update Variables below to work for each different servers.
-ENV1=
-ENV2=
+#Variables
+DEV=`ls /oracle/PS_CFG_HOME | grep "dev"`
+TST=`ls /oracle/PS_CFG_HOME | grep "tst" | grep -v "b$"`
+TSTB=`ls /oracle/PS_CFG_HOME | grep "tst" | grep "b$"`
 
 #Script Start
 
 echo 
-echo -e "Select an Enviroment\n \n 1. $ENV1\n 2. $ENV2\n " 
+echo -e "Select an Enviroment\n \n 1. $DEV\n 2. $TST\n 3. $TSTB\n " 
 echo
 read -p "Your Selection: " CONT
 if [ "$CONT" = "1" ]; then
-        export PS_CFG_HOME='/oracle/PS_CFG_HOME/'$ENV1''
-        export PS_APP_HOME='/oracle/PS_APP_HOME/'$ENV1''
-        export PS_CUST_HOME='/oracle/PS_CUST_HOME/'$ENV1''
-        psadmin 
+        export PS_CFG_HOME='/oracle/PS_CFG_HOME/'$DEV''
+        export PS_APP_HOME='/oracle/PS_APP_HOME/'$DEV''
+        export PS_CUST_HOME='/oracle/PS_CUST_HOME/'$DEV''
+        psadmin
 fi
 if [ "$CONT" = "2" ]; then
-        export PS_CFG_HOME='/oracle/PS_CFG_HOME/'$ENV2''
-        export PS_APP_HOME='/oracle/PS_APP_HOME/'$ENV2''
-        export PS_CUST_HOME='/oracle/PS_CUST_HOME/'$ENV2''
-        psadmin 
+        export PS_CFG_HOME='/oracle/PS_CFG_HOME/'$TST''
+        export PS_APP_HOME='/oracle/PS_APP_HOME/'$TST''
+        export PS_CUST_HOME='/oracle/PS_CUST_HOME/'$TST''
+        psadmin
+fi
+if [ "$CONT" = "3" ]; then
+        export PS_CFG_HOME='/oracle/PS_CFG_HOME/'$TSTB''
+        export PS_APP_HOME='/oracle/PS_APP_HOME/'$TSTB''
+        export PS_CUST_HOME='/oracle/PS_CUST_HOME/'$TSTB''
+        psadmin
 fi
